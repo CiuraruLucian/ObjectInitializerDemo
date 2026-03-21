d1484ffccc8d1787976c400f9a5d572440f1f9e2
Lucian Ciuraru <0ciurg64@solent.ac.uk>
2026-03-21 13:23:46 +0000

    AutoProps Exercises

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            //Make a point by setting each property manually.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+            //Make a point via a custom constructor.
+            Point secondPoint = new Point(20,20);
+            secondPoint.DisplayStats();
+
+            //Make a Point using object init syntax.
+            Point thirdPoint = new Point { X = 30, Y = 30 };
+            thirdPoint.DisplayStats();
+
+            Console.ReadLine();
+        }
+    }
+}
